79aea679b4e84f9734ddb7c8f965b36075663db3
dondycles <johnrod.dondoyano@gmail.com>
2023-09-02 20:53:53 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   app/components/Nav.tsx

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -8,9 +8,9 @@ export default function Nav() {
   const [showMenu, setShowMenu] = useState(false);
   return (
     <motion.nav className="fixed top-0 left-0 w-screen p-6 flex justify-between items-center max-h-24 h-full z-50">
-      <p className="font-black flex items-center text-secondary">
-        <span className="-rotate-2 text-3xl">Heroe</span>
-        <span className=" text-5xl rotate-6 block w-fit mb-2">Z!</span>
+      <p className="font-black flex items-start text-secondary">
+        <span className="text-3xl mt-1">Heroe</span>
+        <span className=" text-5xl block w-fit ">Z!</span>
       </p>
       <MenuBtn
         menuStatus={showMenu}
